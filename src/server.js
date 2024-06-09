958c63e4271b70d359275fa63c3608b3dbe9a241
Boburmirzo <symonbobur@gmail.com>
2024-06-09 13:04:34 +0500

    Deleted unnecessary files and cleaned up the project

--- src/server.js
+++ src/server.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const app = express();
-const { router } = require("./routers");
+const { UserRouter } = require("./routes/users");
+const { CourseRouter } = require("./routes/course");
 app.use(express.json());
-app.use("", router);
+app.use("", CourseRouter);
+app.use("", UserRouter);
 app.listen(3000, () => {
   console.log("Server is running on http://localhost:3000");
 });
